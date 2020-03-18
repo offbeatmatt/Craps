356b193c099e352a6ce372676d1082f932f7258b
offbeatmatt <matthew.richards1@snhu.edu>
2020-03-18 13:29:26 -0400

    Stable run with Roll and Quit option after test values
    Signed-off-by: offbeatmatt <matthew.richards1@snhu.edu>

--- main.cpp
+++ main.cpp
@@ -6,10 +6,33 @@
 
 int main() {
 
+    //Test the die
+    Die dieTest(6, 10,
+                10, "red", false, "Bone");
+
+    std::cout << dieTest.roll() << " " << dieTest.roll() << " " << dieTest.roll() << " " << dieTest.roll() << " "
+              << dieTest.roll() << std::endl;
+    dieTest.setColor("blue");
+    std::cout << dieTest.getColor() << std::endl;
+    dieTest.setCurrentValue(30);
+    std::cout << dieTest.getCurrentValue() << std::endl;
+    dieTest.setIncrement(5);
+    std::cout << dieTest.getIncrement() << std::endl;
+    dieTest.setIsSymbol(true);
+    std::cout << dieTest.getIsSymbol() << std::endl;
+    dieTest.setLowestNumOnDie(0);
+    std::cout << dieTest.getLowestNumOnDie() << std::endl;
+    dieTest.setMaterial("Wood");
+    std::cout << dieTest.getMaterial() << std::endl;
+    dieTest.setNumSides(20);
+    std::cout << dieTest.getNumSides() << std::endl;
+    //End Die Class test
+
+
     std::string userInput = "";
     std::cout << "Roll (r), Bet(b), Quit(e): ";
     std::cin >> userInput;
-    ​
+
     while(userInput != "e") {
         playGame();
         std::cout << "Roll (r), Bet(b), Quit(e): ";
