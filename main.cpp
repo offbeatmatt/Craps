6c25b443c0f7c7cbe1cce1909879763541d1ff81
offbeatmatt <matthew.richards1@snhu.edu>
2020-03-18 12:38:41 -0400

    Die Class 100% coverage
    Framework for game
    
    Signed-off-by: offbeatmatt <matthew.richards1@snhu.edu>

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "Die.h"
 int main() {
+
     Die die1 (6, 10,
               10, "red", false, "Bone");
     std::cout << die1.roll() << " " <<die1.roll() << " " <<die1.roll() << " " <<die1.roll() << " " <<die1.roll() << std::endl;
@@ -18,6 +19,58 @@ int main() {
     std::cout << die1.getMaterial() << std::endl;
     die1.setNumSides(20);
     std::cout << die1.getNumSides() << std::endl;
+
+
+    void playGame();
+    void playFirstRoll(int rollValue);
+    void playSecondRoll(int rollValue);
+    ​
+    int main() {
+        std::string userInput = "";
+        std::cout << "Roll (r), Bet(b), Quit(e): ";
+        std::cin >> userInput;
+        ​
+        while(userInput != "e") {
+            playGame();
+            std::cout << "Roll (r), Bet(b), Quit(e): ";
+            std::cin >> userInput;
+        }
+        return 0;
+    }
+    ​
+    void playGame() {
+        bool firstRoll = true;
+        ​
+        int rollValue = 0;
+        Die die1, die2;
+        rollValue = die1.roll() + die2.roll();
+        ​
+        if(firstRoll) {
+            playFirstRoll(rollValue);
+        } else {
+            playSecondRoll(rollValue);
+        }
+        firstRoll = true;
+    }
+    ​
+    void playFirstRoll(int rollValue){
+        std::cout << rollValue << "\n";
+    };
+    ​
+    void playSecondRoll(int rollValue){
+        std::cout << rollValue << "\n";
+    };
+    Collapse
+
+
+
+
+
+
+
+
+
+
 
     return 0;
 }
